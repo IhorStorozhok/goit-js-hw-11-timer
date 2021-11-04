0dc977adaedc21717ab340264d9fe5692cf16853
IhorStorozhok <storozhokigor@gmail.com>
2021-11-04 21:50:38 +0200

    second try

--- timer.js
+++ timer.js
@@ -1,23 +1,31 @@
-import {
-  pad,
-  secsToDays,
-  secsToHours,
-  secsToMins,
-  secsToSecs,
-} from "./mathOperators.js";
-
-const els = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
-};
-
 class CountdownTimer {
   constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+
+    const pad = (num, qvl) => {
+      return num.toString().padStart(qvl, "0");
+    };
+
+    const secsToDays = (time) =>
+      pad(Math.floor(time / (1000 * 60 * 60 * 24)), 3);
+    const secsToHours = (time) =>
+      pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), 2);
+    const secsToMins = (time) =>
+      pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)), 2);
+    const secsToSecs = (time) =>
+      pad(Math.floor((time % (1000 * 60)) / 1000), 2);
+
+    const box = document.querySelector(this.selector);
+    console.log(box);
+    const els = {
+      days: box.querySelector('[data-value="days"]'),
+      hours: box.querySelector('[data-value="hours"]'),
+      mins: box.querySelector('[data-value="mins"]'),
+      secs: box.querySelector('[data-value="secs"]'),
+    };
+
     if (targetDate > Date.now()) {
-      this.selector = selector;
-      this.targetDate = targetDate;
       const timeTicker = setInterval(() => {
         this.timeDifference = targetDate - Date.now();
         els.days.textContent = secsToDays(this.timeDifference);
@@ -34,5 +42,5 @@ class CountdownTimer {
 
 const timer3 = new CountdownTimer({
   selector: "#timer-3",
-  targetDate: new Date("Dec 17, 2021"),
+  targetDate: new Date("Feb 12, 2022"),
 });
